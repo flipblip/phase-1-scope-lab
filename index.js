d0b6176771c1787aeca7e06ab508a07fabcbc796
fliblip <wickynjy@gmail.com>
2022-08-25 17:03:05 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-// Write your solution in this file!
+var customerName = "bob";
+
+console.log(customerName);
+
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase();
+    return customerName;
+}
+
+var bestCustomer;
+
+function setBestCustomer(){
+    bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob'
+}
+
+const leastFavoriteCustomer = 'wycliffe';
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'Martin';
+    return leastFavoriteCustomer;
+
+}
+
+// function upperCaseCustomerName(){
+//     return customerName.toUpperCase()
+// }
+
+// function setBestCustomer(){
+//     var bestCustomer = 'not bob';
+// }
+
+// overwriteBestCustomer () => {
+//     let bestCustomer = 'Alice'
+// }
+
